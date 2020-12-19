d372c959630f735d71348a04d60fa446254f8eed
RichaPatel14 <50714706+RichaPatel14@users.noreply.github.com>
2020-12-19 16:10:04 +0530

    Update session.php
    remove unnecessary lines and added useful line

--- session.php
+++ session.php
@@ -3,18 +3,6 @@
     { 
         session_start(); 
     } 
-  $conn = mysqli_connect('localhost', 'root','','login');
-   
-   $user_check = $_SESSION['login_user'];
-   
-   $ses_sql = mysqli_query($conn,"select username from register where username = '$user_check' ") or die( mysqli_error($db));
-   
-   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
-   
-   $login_session = $row['username'];
-   
-   if(!isset($_SESSION['login_user'])){
-      header("location:login.php");
-      die();
-   }
+	$user_check = $_SESSION['login_user'];
+  
 ?>
