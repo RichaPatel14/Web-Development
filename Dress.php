9e02237ecaffda4b57e28eafc53457b60e493a69
RichaPatel14 <50714706+RichaPatel14@users.noreply.github.com>
2020-12-19 16:34:12 +0530

    Update Dress.php

--- Dress.php
+++ Dress.php
@@ -26,23 +26,7 @@
 	</head>
 	
 	<body>
-		<div class="container-hd">
-	
-			<div class="header">
-				<span class="logo"><img src="icons/logo/logo.png"></span>
-				<span class="logoname"><p>World shop</p></span>
-				<ul id="topMenu" class="nav">
-					
-	 				<li><a href="home.php">Home</a></li>
-	 				<li><a href="Makeup.php">MakeUp</a></li>
-	 				<li><a href="Dress.php">Dress</a></li>
-					<li><a href="shoes.php">Shoes</a></li>
-					<li><a href="cart.php"><li1><img src="icons/logo/ico-cart.png"></li1><li1>Cart</li1></a></li>
-					<li class="user1"><img src="icons/logo/boy-512.png">Hello! <?php include('session.php'); echo $login_session; ?></li>
-					<li><a href = "logout.php">Sign Out</a></li>
-	 			 </ul>
-			</div>
-		</div>
+		<?php require_once('header.php'); ?>
 		
 		<form action="insertcart.php" method="post">			
 			
@@ -152,30 +136,7 @@
 				<button type="submit" class="btn">Add To Cart</button>
 				</div></div>			
 		</form>
-		<footer>
-				<div class="foot-container">
-			<div class="copyright">
-				<p>&copy; Richa Patel-8334401 </p>
-				<p id="demo">
-			<button type="button" class="btn" onclick="loadDoc()">About US</button>
-				</p>
-			</div>
-		</div>
-				
-	</footer>			
-<script>
-function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", "About.txt", true);
-  xhttp.send();
-}
-</script>
+		<?php require_once('footer.php'); ?>
 		
 	</body>
 </html>
