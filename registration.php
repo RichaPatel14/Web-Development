94de070516a164f23cbc4ac841b1f8b4af677260
RichaPatel14 <50714706+RichaPatel14@users.noreply.github.com>
2020-12-19 16:43:14 +0530

    major update registration.php
    added little encryption( read README file). sort out exception if user face

--- registration.php
+++ registration.php
@@ -1,44 +1,55 @@
 <?php
 
-session_start();
+if(!isset($_SESSION)){
+	session_start();
+}
 
 
 $conn = mysqli_connect('localhost', 'root');
 
 if($conn){
-	echo "connection successful";
+
+	if(mysqli_select_db($conn, 'login')){
+		$code='asdfsd@dasd#';
+		$code=md5($code);
+		$name=$_POST['name'];	
+		$username=$_POST['username'];
+	 		$username=sha1($username); $username=md5($username); $username=crypt($username,$code);
+
+		$email=$_POST['emailid'];
+			$email=sha1($email); $email=md5($email); $email=crypt($email,$username);
+		$password=$_POST['pass'];
+			$password=md5($password); $password=sha1($password); $password=crypt($password,$username);
+		$countrycode=$_POST['countrycode'];
+		$mobile=$_POST['number'];
+		
+		$q="SELECT * FROM `register` WHERE username='$username' ||  email='$email' ";
+
+		$result = mysqli_query($conn, $q) or die(mysqli_error($conn));
+
+		$num =mysqli_num_rows($result);
+
+		if($num==1){
+			echo "<script type='text/javascript'>alert('Already Register');
+			window.location='Registration.html';
+			</script>";
+		}else{
+			$qw="INSERT INTO `register` (`name`, `username`, `email`, `password`, `countrycode`, `mobile`) VALUES ('$name', '$username', '$email', '$password', '$countrycode' , '$mobile')";
+			mysqli_query($conn,$qw);
+			echo "<script type='text/javascript'>alert('Register Successfully');
+			window.location='Verify.html';
+			</script>";
+		}
+	}else{
+		echo "<script type='text/javascript'>alert('Technical Problem, Please try again later'); 
+		window.location='Registration.html';
+		</script> ";
+	}
 }else{
-	echo "connection falied";
-}
-
-mysqli_select_db($conn, 'login');
-
-$username=$_POST['username'];
-$email=$_POST['emailid'];
-$password=$_POST['pass'];
-$mobile=$_POST['number'];
-$website=$_POST['address'];
-
-$q="SELECT * FROM `register` WHERE username='$username' &&  email='$email' ";
-
-
-$result = mysqli_query($conn, $q) or die(mysql_error($conn));
-
-$num =mysqli_num_rows($result);
-
-if($num==1){
-	echo "<script type='text/javascript'>alert('Already Register');
+	echo "<script type='text/javascript'>alert('connection falied! Please try again later');
 	window.location='Registration.html';
 	</script>";
-}else{
-	$qw="INSERT INTO `register` (`username`, `email`, `password`, `mobile`, `website`) VALUES ('$username', '$email', '$password', '$mobile', '$website')";
-	mysqli_query($conn,$qw);
-	echo "<script type='text/javascript'>alert('Register Successfully');
-	window.location='login.html';
-	</script>";
 }
-
-
 
 
 ?>
