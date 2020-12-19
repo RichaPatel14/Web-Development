eb8c32dd23672dd870cdf927d77cdef791e2b9e4
RichaPatel14 <50714706+RichaPatel14@users.noreply.github.com>
2020-12-19 15:56:51 +0530

    Common header file

---
+++ header.php
@@ -0,0 +1,17 @@
+
+<div class="container-hd">
+	<div class="header">
+		<span class="logo"><img src="icons/logo/logo.png"></span>
+		<span class="logoname"><p>World shop</p></span>
+			
+			<ul id="topMenu" class="nav">
+	 			<li><a href="Makeup.php">MakeUp</a></li>
+	 			<li><a href="Dress.php">Dress</a></li>
+				<li><a href="shoes.php">Shoes</a></li>
+				<li><a href="cart.php"><li1><img src="icons/logo/ico-cart.png"></li1><li1>Cart</li1></a></li>
+				<li class="user1"><img src="icons/logo/boy-512.png">Hello! <?php $usr=$_SESSION['username']; echo $usr; ?></li>
+				<li><a href = "logout.php">Sign Out</a></li>
+	 		</ul>
+
+	</div>
+</div>
